fba4d1d9cfdae2d26d2bf4a11fd8177af6782cee
Luis Bacelar <lmbacelar@gmail.com>
2015-01-21 23:07:33 +0000

    added emf function

--- lib/tuscan/iec60584.rb
+++ lib/tuscan/iec60584.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 module Tuscan
   module Iec60584
     extend self
@@ -10,6 +8,16 @@ module Tuscan
     end
     alias_method :t, :t90
     alias_method :temperature, :t90
+
+    def emf t90, type:, a: 0.0, b: 0.0, c: 0.0, d: 0.0, err: 1e-3, num: 10
+      guess = emfr t90, type
+      delta = 1e-2
+      args = { type: type, a: a, b: b, c: c, d: d, err: err, num: num }
+      Rical.inverse_for f: method(:t90), fargs: args, x0: guess - delta, x1: guess + delta,
+                        y: t90, method: :secant, num: num, err: err
+    end
+    alias_method :v, :emf
+    alias_method :voltage, :emf
 
     def emfr t90, type
       raise RangeError, 't90 is outside the valid range' if out_of_range? t90, type
